53151a26140187eb58690ef5fb3daee121eb05e8
Chris LaRocque <larocque.christopher@gmail.com>
2019-05-10 02:26:52 -0400

    What am I even doing?

--- js/main.js
+++ js/main.js
@@ -3,6 +3,11 @@ $.get("nav.html", function(data){
     $("#navbar-frame").replaceWith(data);
 });
 
+/* Loads Video Section Onto Page */
+$.get("video.html", function (data) {
+    $("#video-frame").replaceWith(data);
+});
+
 /* Loads Footer Onto Page */
 $.get("footer.html", function(data){
     $("#footer-frame").replaceWith(data);
